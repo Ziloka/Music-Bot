a7b8558f5f5869aed9af1fa85db3aba5bbe6d40e
572512230701006851 <50429450+572512230701006851@users.noreply.github.com>
2020-01-16 15:19:59 -0500

    editted loop command

--- Commands/Music Cmds/loop.js
+++ Commands/Music Cmds/loop.js
@@ -9,9 +9,8 @@ module.exports = {
         let option = options.find(option => option == args[0])
         if(option == undefined) return message.channel.send('That is not a valid parameter!')
         if(option == 'song'){
-            let newArray = [];
             let currentSong = client.queue.get(message.guild.id).queue[0]
-            client.queue.get(message.guild.id).queue = newArray.push(currentSong)
+            client.queue.get(message.guild.id).queue = [].push(currentSong)
             return message.channel.send('Succesfully looped the song forever')
         } else if(option == 'queue'){
             client.queue.get(message.guild.id).loopQueue = true
