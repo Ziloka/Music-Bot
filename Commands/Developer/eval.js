285b617b72014e578e7141681762bf01c33bc589
572512230701006851 <50429450+572512230701006851@users.noreply.github.com>
2020-01-17 23:51:22 -0500

    Made eval command shorter

--- Commands/Developer/eval.js
+++ Commands/Developer/eval.js
@@ -12,8 +12,7 @@ module.exports = {
     if(developers.find(ID => ID == message.author.id) == undefined) return;
 
     let evaluateThis = args.join(' ');
-    let evaluation = eval(evaluateThis);
-    evaluation = util.inspect(evaluation)
+    let evaluation = util.inspect(eval(evaluateThis))
 
     try{
     let evaled = new Discord.RichEmbed();
