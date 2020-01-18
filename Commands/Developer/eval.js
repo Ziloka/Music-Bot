4de8b8fc87cc06597c8e84079eff4480b499eb10
572512230701006851 <50429450+572512230701006851@users.noreply.github.com>
2020-01-17 23:50:00 -0500

    Changed evaluation command

--- Commands/Developer/eval.js
+++ Commands/Developer/eval.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const util = require('util');
+const fetch = require('node-fetch');
 const {developers} = require('./../../index.js');
 
 module.exports = {
@@ -9,22 +11,46 @@ module.exports = {
 
     if(developers.find(ID => ID == message.author.id) == undefined) return;
 
-    try{
     let evaluateThis = args.join(' ');
-    let evaulation = eval(evaluateThis);
+    let evaluation = eval(evaluateThis);
+    evaluation = util.inspect(evaluation)
+
+    try{
     let evaled = new Discord.RichEmbed();
     evaled.setTitle('Evaluation')
     evaled.setColor('#CDDC39')
     evaled.addField('Input', `\`\`\`js
 ${evaluateThis}\`\`\``)
     evaled.addField('Output', `\`\`\`js
-${evaulation}\`\`\``)
+${evaluation}\`\`\``)
     evaled.addField('Type', `\`\`\`js
-${typeof evaulation}\`\`\``)
+${typeof evaluation}\`\`\``)
     evaled.setFooter(`Evaluated by ${message.author.username}`, message.author.displayAvatarURL)
     return message.channel.send({embed: evaled})
     }catch(e){
-        return message.channel.send(e.message)
+        if(e.message == "RichEmbed field values may not exceed 1024 characters."){
+            let options = {
+                url: "https://hasteb.in",
+                extension: "js"
+            }
+            let res = await fetch(`${options.url}/documents`, {
+                method: "POST",
+                body: evaluation.slice(8),
+                headers: { "Content-Type": "text/plain" }
+            })
+            if(!res.ok) message.channel.send(res.statusText)
+            res.json().then(newRes => {
+                let over1024Characters = new Discord.RichEmbed()
+                over1024Characters.setTitle('Output')
+                over1024Characters.setDescription(`The output was longer than 1024 characters so I have placed the evaluation in a text storage site:\n ${`https://hasteb.in/${newRes.key}.${options.extension}`}`)
+                over1024Characters.setFooter(`Evaluated by ${message.author.username}`, message.author.displayAvatarURL)
+                return message.channel.send({embed: over1024Characters})
+                
+            })
+            
+        } else {
+            return message.channel.send(e.message)
+        }
     }
 
     }
