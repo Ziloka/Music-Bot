cbaf4e247804c0c62a1b58cecf9d341b1e6e2bb8
572512230701006851 <50429450+572512230701006851@users.noreply.github.com>
2020-01-18 21:00:17 -0500

    added setAvatar command

---
+++ Commands/Developer/setAvatar.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const {developers} = require('../../index.js');
+
+module.exports = {
+    name: "setavatar",
+    usage: "setavatar <attachment>",
+    argRequirements: args => !!args.length,
+    run: async (client, message, args) => {
+
+        if(developers.find(ID => ID == message.author.id) == undefined) return;
+
+        if(message.attachments.size == 0) return message.channel.send('I cannot send my profile picture to nothing!')
+        if(message.attachments.size == 1){
+            let pfp = message.attachments.first().url
+            client.user.setAvatar(pfp).then(() => {
+                return message.channel.send('Succesfully changed Avatar')
+            })
+        } else if(args[0].includes('https://')){
+           let url = args[0]
+           try{
+                client.user.setAvatar(url).then(() => {
+                    return message.channel.send('Succesfully changed Avatar')
+                })
+           }catch(e){
+               return message.channel.send(e)
+           }
+        } else {
+            return message.channel.send("I cannot set my profile picture to nothing!")
+        }
+
+    }
+}
